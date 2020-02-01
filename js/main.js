ac4a4a90828236576bd5ffb49fa3d3d12afaf6b1
onetaro <onetaro1@gamil.com>
2020-02-01 18:30:49 +0900

    クイズマークアップ

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+{
+  const question = document.getElememtById('question');
+  const choices = document.getElememtById('choices');
+  const btn = document.getElememtById('btn');
+
+  const quizSet = [
+    {q:'what is A?',c:['A01','A02','A03']},
+    {q:'what is B?',c:['B01','B02','B03']},
+    {q:'what is C?',c:['C01','C02','C03']},
+  ];
+
+  let currentNum = 0;
+}
